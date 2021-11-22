30ef1be9f5b38ccdb7deace75608aa482caa7d99
Ezekiel Halley <eze.halley@gmail.com>
2021-11-21 20:04:33 -0500

    Update MainActivity.java

--- app/src/main/java/edu/towson/cosc412/martindale/gymrat/MainActivity.java
+++ app/src/main/java/edu/towson/cosc412/martindale/gymrat/MainActivity.java
@@ -7,7 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+// DELETE THIS COMMENT
 public class MainActivity extends AppCompatActivity {
 
     private Button loginButton;
