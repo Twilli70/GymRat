53a550fc76d4aebca4718a5d89b6d2b0dda50a16
Twilli70 <92907346+Twilli70@users.noreply.github.com>
2021-12-20 14:26:42 -0500

    test1

--- app/src/main/java/edu/towson/cosc412/martindale/gymrat/ui/activities/MainActivity.java
+++ app/src/main/java/edu/towson/cosc412/martindale/gymrat/ui/activities/MainActivity.java
@@ -13,9 +13,12 @@ import android.widget.Toast;
 
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
 import edu.towson.cosc412.martindale.gymrat.R;
 import edu.towson.cosc412.martindale.gymrat.database.GymRatDB;
+import edu.towson.cosc412.martindale.gymrat.database.entities.Exercise;
+import edu.towson.cosc412.martindale.gymrat.database.entities.Workout;
 import edu.towson.cosc412.martindale.gymrat.old.DBHelper;
 
 public class MainActivity extends AppCompatActivity {
@@ -29,6 +32,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         GymRatDB db = GymRatDB.getInstance();
+        new Thread(() -> {
+            Looper.prepare();
+            ArrayList<Workout> workoutArrayList1 = new ArrayList<Workout>();
+            Workout w1 = new Workout();
+            w1.reps = 12;
+            w1.sets = 3;
+            Exercise ex1 = new Exercise();
+            ex1.name = "Squats";
+            ex1.caloriesPerMinute = 5;
+            ex1.equipmentID = 2;
+            ex1.estimateTime = 20;
+            ex1.targetBodyPart = "Legs";
+            w1.exercise = ex1;
+            w1.breakTime = 30;
+            workoutArrayList1.add(w1);
+           db.addNewRoutine("DayTwo", workoutArrayList1 );
+        }).start();
+
+        new Thread(() -> {
+            Looper.prepare();
+            ArrayList<Workout> workoutArrayList1 = new ArrayList<Workout>();
+            Workout w1 = new Workout();
+            w1.reps = 12;
+            w1.sets = 3;
+            Exercise ex1 = new Exercise();
+            ex1.name = "Squats";
+            ex1.caloriesPerMinute = 5;
+            ex1.equipmentID = 2;
+            ex1.estimateTime = 20;
+            ex1.targetBodyPart = "Legs";
+            w1.exercise = ex1;
+            w1.breakTime = 30;
+            workoutArrayList1.add(w1);
+            db.addNewRoutine("DayTwo", workoutArrayList1 );
+        }).start();
 
         //reference layout
         loginBtn= findViewById(R.id.loginBtn);
